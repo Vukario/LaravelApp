2239ba8ceda10d509bc60cc2ebe370f9a24a46b4
Vukario <93868787+Vukario@users.noreply.github.com>
2022-06-20 21:56:38 +0200

    Add files via upload

---
+++ storage/framework/views/2756a4ff6f11f6affae5de97d9a9dce7b1521d98.php
@@ -0,0 +1,79 @@
+<header class="max-w-xl mx-auto mt-20 text-center">
+    <h1 class="text-4xl">
+        Latest <span class="text-blue-500">Laravel From Scratch</span> News
+    </h1>
+
+    <h2 class="inline-flex mt-2">By Lary Laracore <img src="public/images/lary-head.svg"
+                                                       alt="Head of Lary the mascot"></h2>
+
+    <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-4">
+        <!--  Category -->
+        <div class="relative lg:inline-flex bg-gray-100 rounded-xl">
+            <?php if (isset($component)) { $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4 = $component; } ?>
+<?php $component = $__env->getContainer()->make(Illuminate\View\AnonymousComponent::class, ['view' => 'components.dropdown','data' => []]); ?>
+<?php $component->withName('dropdown'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php $component->withAttributes([]); ?>
+                 <?php $__env->slot('trigger', null, []); ?> 
+                    <button class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex">
+                        <?php echo e(isset($currentCategory) ? ucwords($currentCategory->name)  : 'Categories'); ?>
+
+                        <?php if (isset($component)) { $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4 = $component; } ?>
+<?php $component = $__env->getContainer()->make(Illuminate\View\AnonymousComponent::class, ['view' => 'components.icon','data' => ['name' => 'down-arrow','class' => 'absolute pointer-events-none','style' => 'right: 12px;']]); ?>
+<?php $component->withName('icon'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php $component->withAttributes(['name' => 'down-arrow','class' => 'absolute pointer-events-none','style' => 'right: 12px;']); ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4)): ?>
+<?php $component = $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4; ?>
+<?php unset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4); ?>
+<?php endif; ?>
+                    </button>
+                 <?php $__env->endSlot(); ?>
+
+                <?php if (isset($component)) { $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4 = $component; } ?>
+<?php $component = $__env->getContainer()->make(Illuminate\View\AnonymousComponent::class, ['view' => 'components.dropdown-item','data' => ['href' => '/example-app/server.php']]); ?>
+<?php $component->withName('dropdown-item'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php $component->withAttributes(['href' => '/example-app/server.php']); ?>All <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4)): ?>
+<?php $component = $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4; ?>
+<?php unset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4); ?>
+<?php endif; ?>
+
+                <?php $__currentLoopData = $categories; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $category): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <?php if (isset($component)) { $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4 = $component; } ?>
+<?php $component = $__env->getContainer()->make(Illuminate\View\AnonymousComponent::class, ['view' => 'components.dropdown-item','data' => ['href' => '/categories/'.e($category->slug).'','active' => request()->is('categories/' . $category->slug)]]); ?>
+<?php $component->withName('dropdown-item'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php $component->withAttributes(['href' => '/categories/'.e($category->slug).'','active' => \Illuminate\View\Compilers\BladeCompiler::sanitizeComponentAttribute(request()->is('categories/' . $category->slug))]); ?><?php echo e(ucwords($category->name)); ?> <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4)): ?>
+<?php $component = $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4; ?>
+<?php unset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4); ?>
+<?php endif; ?>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+             <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4)): ?>
+<?php $component = $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4; ?>
+<?php unset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4); ?>
+<?php endif; ?>
+        </div>
+
+        <!-- Search -->
+        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
+            <form method="GET" action="#">
+                <input type="text" name="search" placeholder="Find something"
+                       class="bg-transparent placeholder-black font-semibold text-sm">
+            </form>
+        </div>
+    </div>
+</header>
+<?php /**PATH D:\NOTEBOOK\LARAVEL\example-app\resources\views\_posts-header.blade.php ENDPATH**/ ?>
